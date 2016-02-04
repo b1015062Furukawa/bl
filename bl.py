096d0aa95e0a0cebf94255c9383cb4a4bac7a0a5
Uehara25 <akiya.noface@gmail.com>
2016-02-04 14:13:03 +0900

    コマンドライン引数を実装
    詳細は -h で見なさい

--- bl.py
+++ bl.py
@@ -1,8 +1,52 @@
-import requests, sys, re
+import requests, sys, re, argparse
+from station_dict import get_station_dict
 from bs4 import BeautifulSoup as bs
+from option import Option
 
-def getsrc(_in=165, _out=155):
-    r = requests.get("http://www.hakobus.jp/result.php?in={0}&out={1}".format(_in, _out))
+def parse_args():
+    parser = argparse.ArgumentParser()
+    f_help =    """
+                set entraining point. default: fun
+                """
+    parser.add_argument("-f", help=f_help, default="fun")
+
+    t_help =    """
+                set destination point. default: kameda
+                """
+    parser.add_argument("-t", help=t_help, default="kameda")
+    parser.add_argument("--list", help="list up station names", action="store_true")
+
+    args = parser.parse_args()
+    return args
+
+def convert_name_to_number(name):
+    station_dict = get_station_dict()
+
+    ret = ""
+    try:
+        ret = station_dict.get(name)
+    except:
+        print(name + " is not existed in dict", file=sys.stderr)
+        sys.exit(1)
+
+    return ret
+
+def get_option():
+    args = parse_args()
+    option = Option()
+    option.f = convert_name_to_number(args.f)
+    option.t = convert_name_to_number(args.t)
+
+    # not implemented
+    if args.list:
+        option.list = args.list
+    option.a = False
+
+
+    return option
+
+def getsrc(option):
+    r = requests.get("http://www.hakobus.jp/result.php?in={0}&out={1}".format(option.f, option.t))
     return r.text.encode("Shift_JIS", 'ignore').decode("utf-8", 'ignore')
 
 def get_next_bus_time(src):
@@ -32,10 +76,17 @@ def get_next_bus_time(src):
 
     return left_minutes
 
-if __name__ == "__main__":
+def main():
+    option = get_option()
+
+    if option.list:
+        dct = get_station_dict()
+        for key in dct.keys():
+            print(key)
+        sys.exit(0)
 
     try:
-        src = getsrc()
+        src = getsrc(option)
     except ConnectionError:
         print("Connection Error!", file=sys.stderr)
     except Exception as e:
@@ -44,3 +95,6 @@ if __name__ == "__main__":
     left_minutes = get_next_bus_time(src)
     for minute in left_minutes:
         print(minute)
+
+if __name__ == "__main__":
+    main()
---
+++ station_dict.py
@@ -0,0 +1,6 @@
+def get_station_dict():
+    station_dict = {
+        "fun": 165,
+        "kameda": 155
+    }
+    return station_dict
